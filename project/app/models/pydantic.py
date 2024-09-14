48b1f9090595389b152d2a5c51d4808e6325d2d5
YangWu1227 <yangwu2020@gmail.com>
2024-09-14 00:51:13 -0400

    feat: pydantic and tortoise models

---
+++ project/app/models/pydantic.py
@@ -0,0 +1,13 @@
+from pydantic import BaseModel 
+
+class SummaryPayloadSchema(BaseModel):
+    """
+    The request body containing the url for which to generate a text summary.
+    """
+    url: str
+
+class SummaryResponseSchema(SummaryPayloadSchema):
+    """
+    The response model containing the generated id of the text summary.
+    """
+    id: int
---
+++ project/app/models/tortoise.py
@@ -0,0 +1,27 @@
+from tortoise import fields
+from tortoise.models import Model
+from tortoise.contrib.pydantic import pydantic_model_creator
+
+class TextSummary(Model):
+    """
+    A data model representing a summarized text of a given URL. 
+    """
+    id = fields.IntField(primary_key=True)
+    url = fields.TextField()
+    summary = fields.TextField()
+    # Automatically set the field to now when the object is first created
+    created_at = fields.DatetimeField(auto_now_add=True)
+
+    def __str__(self) -> str:
+        """
+        Returns the URL as a string representation of the object.
+
+        Returns
+        -------
+        str
+            The URL of the text summary.
+        """
+        return self.url
+
+# Build a Pydantic Model off Tortoise Model
+SummarySchema = pydantic_model_creator(TextSummary)
