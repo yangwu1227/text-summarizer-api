c62a817eacf95105f72b8c6a4560ae8a382aee5d
YangWu1227 <yangwu2020@gmail.com>
2024-09-14 21:41:20 -0400

    feat: shell scripts to build production image and deploy to heroku

---
+++ project/scripts/build_prod_image.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+# Exit immediately if any command fails
+set -e
+
+# Prompt the user for the location of the production Dockerfile and build context
+read -p "Enter the relative or absolute path to the production Dockerfile: " dockerfile_path
+read -p "Enter the relative or absolute path to the production build context: " build_context_path
+# Prompt the user for the target platform
+read -p "Enter the target platform (e.g., linux/amd64, linux/arm64, or both): " target_platforms
+# Heroku app name
+read -p "Enter the name of the Heroku app: " app_name
+
+# Check if the Dockerfile and build context exist
+if [ -f "$dockerfile_path" ] && [ -d "$build_context_path" ]; then
+
+    # Build the production image
+    image_tag="registry.heroku.com/$app_name/web"
+    echo "Building the production image..."
+    DOCKER_BUILDKIT=1 docker buildx build --platform "$target_platforms" -f "$dockerfile_path" -t "$image_tag" "$build_context_path"
+    echo "Docker image built successfully!"
+else
+    echo "The specified Dockerfile or build context does not exist"
+    exit 1
+fi
+
+echo "The production image tag is: $image_tag:latest"
